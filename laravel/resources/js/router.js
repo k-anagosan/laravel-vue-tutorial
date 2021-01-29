1607b97a54cd6b457b7fead51dc8b8b94e3371c1
k-shishido <anagosan.1027@gmail.com>
2021-01-29 21:31:38 +0900

    add navigation guard

--- laravel/resources/js/router.js
+++ laravel/resources/js/router.js
@@ -4,22 +4,31 @@ import VueRouter from "vue-router";
 import PhotoList from "./pages/PhotoList.vue";
 import Login from "./pages/Login.vue";
 
+import store from "./store";
+
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: "/",
-        component: PhotoList
+        component: PhotoList,
     },
     {
         path: "/login",
-        component: Login
-    }
+        component: Login,
+        beforeEnter(to, from, next) {
+            if (store.getters["auth/check"]) {
+                next("/");
+            } else {
+                next();
+            }
+        },
+    },
 ];
 
 const router = new VueRouter({
     mode: "history",
-    routes
+    routes,
 });
 
 export default router;
